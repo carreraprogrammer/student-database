ed6074fd86385188f76c09fd5f0feb76cb5b8a99
Daniel <daniel.carrera@dataico.com>
2023-06-11 15:12:20 -0500

    Finish tables schema

--- schema.sql
+++ schema.sql
@@ -27,3 +27,17 @@ ALTER TABLE majors ADD COLUMN major_id SERIAL PRIMARY KEY;
 ALTER TABLE majors ADD COLUMN major VARCHAR(50) NOT NULL;
 
 ALTER TABLE students ADD FOREIGN KEY(major_id) REFERENCES majors(major_id);
+
+ALTER TABLE courses ADD COLUMN course_id SERIAL PRIMARY KEY;
+
+ALTER TABLE courses ADD COLUMN course VARCHAR(100) NOT NULL;
+
+ALTER TABLE majors_courses ADD COLUMN major_id INT;
+
+ALTER TABLE majors_courses ADD FOREIGN KEY(major_id) REFERENCES majors(major_id);
+
+ALTER TABLE majors_courses ADD COLUMN course_id INT;
+
+ALTER TABLE majors_courses ADD FOREIGN KEY(course_id) REFERENCES courses(course_id);
+
+ALTER TABLE majors_courses ADD PRIMARY KEY(major_id, course_id);
