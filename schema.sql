1c1e3c735b937da5b543df42544baaee4ae171ac
Daniel <daniel.carrera@dataico.com>
2023-06-11 14:50:30 -0500

    add column to student table

--- schema.sql
+++ schema.sql
@@ -9,3 +9,15 @@ CREATE TABLE majors();
 CREATE TABLE courses();
 
 CREATE TABLE majors_courses();
+
+ALTER TABLE students ADD COLUMN student_id SERIAL PRIMARY KEY;
+
+ALTER TABLE students ADD COLUMN first_name VARCHAR(50) NOT NULL;
+
+ALTER TABLE students ADD COLUMN last_name  VARCHAR(50) NOT NULL;
+
+ALTER TABLE students ADD COLUMN major_id INT;
+
+ALTER TABLE students ADD COLUMN major_id INT;
+
+ALTER TABLE students ADD COLUMN gpa NUMERIC(2,1);
