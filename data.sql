b40f090da3f9d8d8381a0e685efef6aea11874df
Daniel <daniel.carrera@dataico.com>
2023-06-11 18:01:03 -0500

    Truncate tables

--- data.sql
+++ data.sql
@@ -7,3 +7,7 @@ INSERT INTO courses(course) VALUES('Data Structures and Algorithms');
 INSERT INTO majors_courses(major_id, course_id) VALUES(1, 1);
 
 INSERT INTO students(first_name, last_name, major_id, gpa) VALUES('Rhea', 'Kellems', 1, 2.5);
+
+TRUNCATE courses, majors_courses;
+
+TRUNCATE majors, majors_courses, students;
---
+++ insert_data.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+#Script to insert data from courses.csv and students.csv into students database
