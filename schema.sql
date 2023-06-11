b0fb1370bfcb6a4e9a191c44f6ed00052b3024b5
Daniel <daniel.carrera@dataico.com>
2023-06-11 14:35:23 -0500

    Create students database and his tables

--- schema.sql
+++ schema.sql
@@ -1,5 +1,11 @@
 /* Database schema to keep the structure of entire database. */
 
-CREATE TABLE animals (
-    name varchar(100)
-);
+CREATE DATABASE students;
+
+CREATE TABLE students();
+
+CREATE TABLE majors();
+
+CREATE TABLE courses();
+
+CREATE TABLE majors_courses();
